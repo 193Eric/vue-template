299e4203cd0315786d2a59a3f38259f17b3995d7
eric <likai@pandaminer.com>
2020-07-20 15:23:38 +0800

    修改模板

--- template/src/mock/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * all mock data
- */
-import Mock from 'mockjs'
-Mock.setup({
-  timeout: 400
-})
-// 中升公众号2.0首页
-import './store/index'
--- template/src/wechat/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import Wechat from '../plugins/wechat'
-import config from './config'
-
-Vue.use(Wechat)
-export default new Wechat(config)
